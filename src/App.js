c2b227630f53eb670840b44f3ca3eb993622adb4
SanaAlvi <sanaalvi066@gmail.com>
2020-06-09 19:22:12 +0500

    Create dinner.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'; // we are importing React function from reat 
+import './App.css';
+import Dinner from './Dinner.js'
+
+function App() {
+  return  (
+    <div className="App">
+      <Dinner dishName="Chicken Bariyani" sweetDish="Kheer" />
+
+      <hr />
+      <Dinner dishName="Chicken Bariyani" sweetDish="Kheer" />
+
+      <hr />
+      <Dinner dishName="Chicken Bariyani" sweetDish="Kheer" />
+      
+    </div>
+  )
+ }
+
+export default App; //first we have to export then in other file it can be import
