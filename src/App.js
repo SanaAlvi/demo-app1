4eaa1b9a56b70bd32c819cc4dd0aa49546235030
SanaAlvi <sanaalvi066@gmail.com>
2020-06-10 23:44:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from 'react'; // we are importing React function from reat 
+import React, { useState } from 'react'; // we are importing React function from reat 
 import './App.css';
 import Dinner from './dinner.js'
 
 function App() {
+  let [count,Countset]=useState(0)
   return  (
     <div className="App-header">
          <h1>Sana Alvi Shafqat</h1>
-        <Dinner dishName="Karahi" sweetDish="Halwa"  className="App" />
+        <Dinner dishName="Karahi" sweetDish="Halwa" />
+        <button onClick={()=> Countset(count+1)}>update State</button>
     </div>
   )
+  
  }
 
-export default App; //first we have to export then in other file it can be import
+export default App; //First we have to export then in other file it can be import
