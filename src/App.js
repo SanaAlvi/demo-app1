2f3fd5207f2a87770e6899a92aa6ca4a04685004
SanaAlvi <sanaalvi066@gmail.com>
2020-06-10 23:59:03 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { useState } from 'react'; // we are importing React function from reat 
 import './App.css';
 import Dinner from './dinner.js'
+import morning from './morning.js'
 
 function App() {
   let [count,Countset]=useState(0)
+  let [ismorning,setmorning]=useState(true)
+
   return  (
     <div className="App-header">
          <h1>Sana Alvi Shafqat</h1>
         <Dinner dishName="Karahi" sweetDish="Halwa" />
         <h3>The updated value is {count} </h3>
         <button onClick={()=> Countset(count+1)}>update State</button>
+        <button onClick={()=> morning ? "Mrning" : "Night"}>
+         Change Time 
+        </button>
     </div>
   )
   
