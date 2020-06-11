ebe06772f9d97df01d99413ab9311d55f669b228
SanaAlvi <sanaalvi066@gmail.com>
2020-06-11 13:45:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   let [isMorning,setMorning]=useState(true)
 
   return  (
-    <div className={`App ${isMorning ? 'App-header' : ''}`}>
+    <div className={`App-header ${isMorning ? 'App' : ''}`}>
   
          <h1>Sana Alvi</h1>
 
